d9d44fcdf63a2488cf588fefa6b34e8c3c4b3548
pocoyo <67566122+Pocoyo-dev@users.noreply.github.com>
2022-01-15 13:45:17 -0500

    time debugging

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,18 +7,36 @@ const commands = [];
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
+	let duration1 = end1();
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
+	duration1 = duration1 * 1000;
+    duration1 = Math.round(duration1);
+    duration1 = duration1 + 1;
+    duration1 = duration1 / 1000;
+    console.emoji(`✅ `, `${file} took ${duration1} ms to register` )
 }
 const commandFilesMc = fs.readdirSync('./commands/mc').filter(file => file.endsWith('.js'));
 for (const file of commandFilesMc) {
+	let duration1 = end1();
 	const command = require(`./commands/mc/${file}`);
 	commands.push(command.data.toJSON());
+	duration1 = duration1 * 1000;
+    duration1 = Math.round(duration1);
+    duration1 = duration1 + 1;
+    duration1 = duration1 / 1000;
+    console.emoji(`✅ `, `${file} took ${duration1} ms to register` )
 }
 const commandFilesOther = fs.readdirSync('./commands/others').filter(file => file.endsWith('.js'));
 for (const file of commandFilesOther) {
+	let duration1 = end1();
 	const command = require(`./commands/others/${file}`);
 	commands.push(command.data.toJSON());
+	duration1 = duration1 * 1000;
+    duration1 = Math.round(duration1);
+    duration1 = duration1 + 1;
+    duration1 = duration1 / 1000;
+    console.emoji(`✅ `, `${file} took ${duration1} ms to register` )
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
